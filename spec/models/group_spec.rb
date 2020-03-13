4b44d98f8fd0fc9128a51d313f8e7ea3d3c0b192
shin usuda <sasasa.24.blue@gmail.com>
2020-03-13 21:44:19 +0900

    Add group rspec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Group do
+  describe '#create' do
+    it 'is valid' do
+      user = create(:user)
+      group = build(:group, user_id: user.id)
+      expect(group).to be_valid
+    end
+
+    it 'is invalid without title' do
+      user = create(:user)
+      group = build(:group, title: "", user_id: user.id)
+      group.valid?
+      expect(group.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without user_id' do
+      user = create(:user)
+      group = build(:group)
+      group.valid?
+      expect(group.errors[:user]).to include("must exist")
+    end
+  end
 end
