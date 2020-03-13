43c5d333ee0ad538e56b04dd256ef829e2b92b62
shin usuda <sasasa.24.blue@gmail.com>
2020-03-13 17:38:00 +0900

    Fix user & group model association

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations',
                                    sessions: 'users/sessions'}
-  root to: 'groups#index'
   get 'users/current'
+  resources :groups, only: [:index]
+  root to: 'groups#index'
 end
--- db/migrate/20200313071055_create_groups.rb
+++ db/migrate/20200313071055_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
-      t.string :title, null: false
+      t.string     :title, null: false
+      t.references :user,  foreign_key: true
       t.timestamps
     end
   end
